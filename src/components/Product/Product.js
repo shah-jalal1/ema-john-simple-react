e643c9175b8ae9df266ad49ab4f693ce6fe2e60c
shahjalalcse <shah-jalal1>
2021-02-26 22:30:33 +0600

    handleAddProduct

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './product.css'
 
 const Product = (props) => {
-    console.log(props.product);
+    // console.log(props.product);
     const {img, name, seller, price,stock} = props.product;
     return (
         <div className="product">
@@ -17,7 +17,9 @@ const Product = (props) => {
             <p>${price}</p>
             <br/>
             <p><small>Only {stock} left in stock - order soon</small></p>
-            <button className="main-button"><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+            <button 
+            onClick={() => props.handleAddProduct(props.product)}
+            className="main-button"><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
             </div>
         </div>
     );
