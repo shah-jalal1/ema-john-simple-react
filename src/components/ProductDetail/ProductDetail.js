7a92e111951cc39a97482f7a1d2c6ab5ee6ca39d
shahjalalcse <shah-jalal1>
2021-03-29 00:51:09 +0600

    fake data remove and fetch from database

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-// import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 
 const ProductDetail = () => {
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-// import fakeData from '../../fakeData';
 import { useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
@@ -8,7 +7,7 @@ import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseMana
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
-    // const first10 = fakeData.slice(0,10);
+
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
 
@@ -20,19 +19,19 @@ const Shop = () => {
 
     document.title = "Shop More";
 
-    useEffect(() => {
+    useEffect(()=>{
         const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart);
-        console.log(products, productKeys);
-        if (products.length > 0) {
-            const previousCart = productKeys.map(existingKey => {
-                const product = products.find(pd => pd.key === existingKey);
-                product.quantity = savedCart[existingKey];
-                return product;
-            })
-            setCart(previousCart);
-        }
-    }, [products])
+        fetch('http://localhost:5000/productsByKeys', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(productKeys)
+        })
+        .then(res => res.json())
+        .then(data => setCart(data))
+    }, [])
 
     const handleAddProduct = (product) => {
         const toBeAddedKey = product.key;
