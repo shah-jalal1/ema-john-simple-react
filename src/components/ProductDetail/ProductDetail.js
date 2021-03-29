b35dd98afc72aa6559ddbe2915f0cff390468c60
shahjalalcse <shah-jalal1>
2021-03-29 13:04:19 +0600

    done

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -7,7 +7,7 @@ const ProductDetail = () => {
     const [product, setProduct] = useState({});
 
     useEffect(() => {
-        fetch('http://localhost:5000/product/'+ productKey)
+        fetch('https://powerful-sierra-54029.herokuapp.com/product/'+ productKey)
         .then(res => res.json())
         .then(data => setProduct(data));
     }, [productKey])
